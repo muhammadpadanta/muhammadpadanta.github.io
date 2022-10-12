b0e267fb3b3857bdf0964f039a65ee5e29c94d05
muhammadpadanta <115600378+muhammadpadanta@users.noreply.github.com>
2022-10-12 09:20:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 0
+
+function increment () {
+  
+count = count + 1
+document.getElementById("count-el").innerText = count
+console.log (count)
+}
+
+
+//document.getElementById("count-el").innerText = innerText + 1
+
+// let innerText = document.getElementById('count-el').innerText ;
+// innerText++ ;
+// document.getElementById('count-el').innerText = innerText
+// console.log (innerText)
